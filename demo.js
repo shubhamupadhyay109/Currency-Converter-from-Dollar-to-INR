c1d1651e60dc3f105e9470c455b934d8f6bc23da
shubhamu upadhyay <shubhamupadhyay109@gmail.com>
2020-05-18 23:07:33 +0530

    Initial Commit

--- demo.js
+++ demo.js
@@ -1,29 +1,64 @@
 const fs = require('fs');
 var multer = require('multer');
 const path = require("path") 
+const fetch = require("node-fetch") 
 const {Parser} = require('json2csv');
 const csvjson = require("csvjson") 
 const express=require("express");
 const app=express();
 const bodyParser=require("body-parser");
 //const json2csvParser = new Parser()
-var values=[];
-let arr=[ { Amounts: 75 },
-    { Amounts: 150 },
-    { Amounts: 225 },
-    { Amounts: 300 },
-    { Amounts: 375 },
-    { Amounts: 450 },
-    { Amounts: 525 },
-    { Amounts: 600 },
-    { Amounts: 675 },
-    { Amounts: 750 },
-    { Amounts: 825 },
-    { Amounts: 900 } ]
+fetch('https://api.exchangeratesapi.io/latest?base=USD')
+                .then(val=>{
+                    console.log(val.body)
+                    jsonobj.forEach((e)=>{
+                    values.push({"Amounts":(e.Amounts)*75})
+                })
+                })
 
-const json2csvParser = new Parser();
-const csv = json2csvParser.parse(arr);
-fs.writeFile("demo.csv",csv,(err,val)=>{
-    if(err) throw err;
-    console.log(val)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var values=[];
+// let arr=[ { Amounts: 75 },
+//     { Amounts: 150 },
+//     { Amounts: 225 },
+//     { Amounts: 300 },
+//     { Amounts: 375 },
+//     { Amounts: 450 },
+//     { Amounts: 525 },
+//     { Amounts: 600 },
+//     { Amounts: 675 },
+//     { Amounts: 750 },
+//     { Amounts: 825 },
+//     { Amounts: 900 } ]
+
+// const json2csvParser = new Parser();
+// const csv = json2csvParser.parse(arr);
+// fs.writeFile("demo.csv",csv,(err,val)=>{
+//     if(err) throw err;
+//     console.log(val)
+// })
+
+
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 var multer = require('multer');
 const path = require("path") 
 const {Parser} = require('json2csv');
-const csvjson = require("csvjson") 
+const csvjson = require("csvjson")
+var nodemailer = require('nodemailer');
+const fetch = require("node-fetch") 
 const express=require("express");
 const app=express();
 const bodyParser=require("body-parser");
@@ -11,35 +13,38 @@ var values=[];
 app.use(bodyParser.urlencoded({extended:true}));
     
 var storage = multer.diskStorage({ 
-    destination: function (req, file, cb) { 
+    destination: (req, file, cb)=> { 
         // Uploads is the Upload_folder_name 
         cb(null, "csv") 
     }, 
-    filename: function (req, file, cb) { 
+    filename: (req, file, cb)=> { 
         cb(null, file.fieldname + "-" + Date.now()+".csv") 
     } 
 }) 
  
 var upload = multer({storage: storage}).single("csv_file");        
 
-app.get("/upload_csv",  function(req,res) {
-	res.sendfile(__dirname+"/html/index.html");
+app.get("/upload_csv", (req,res)=> {
+	res.sendFile(__dirname+"/html/index.html");
 })
  
-app.post("/upload_csv/",function (req, res, next) { 
+app.post("/upload_csv/", (req, res)=> { 
     upload(req,res,function(err) { 
         if(err) { 
-            res.send(err) 
+            res.send(err.message) 
         } 
         else { 
             console.log("success. File Uploaded !!");
-            
            const filename=__dirname+"/csv/"+res.req.file.filename
             fs.readFile(filename,'utf-8',(err,val)=>{
                 if (err) throw err.message;
                 jsonobj=csvjson.toObject(val)
-                jsonobj.forEach((e)=>{
-                    values.push({"Amounts":(e.Amounts)*75})
+                fetch('https://api.exchangeratesapi.io/latest?base=USD')
+                .then(val=>val.json())
+                .then(val=>{
+                    jsonobj.forEach((e)=>{
+                    values.push({"Amounts":(e.Amounts)*(val.rates.INR)})
+                    })
                 })
            });
         setTimeout(() => {
@@ -53,9 +58,41 @@ app.post("/upload_csv/",function (req, res, next) {
         } 
     }) 
 })
-// app.get("/process",function(req,res,next) {
-//     console.log(res.file.filename)
-// })
+app.get("/email",function(req,res) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'XXXXXXXXX@gmail.com',
+          pass: 'XXXXXXX'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'XXXXXXXXX110@gmail.com',
+        to: 'XXXXXXX@XXXXX.com',
+        subject: 'Sending Email using Node.js',
+        text: `Hi Smartherd, thank you for your nice Node.js tutorials.
+                I will donate 50$ for this course. Please send me payment options.`,
+        attachments:[
+            {
+                filename:"demo1589822081969.csv",
+                path:`${__dirname}/processcsv/demo1589822081969.csv`,
+                cid:"shubham316@"
+            }
+        ],        
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.send('Email sent successfully')
+          transporter.close();
+        }
+      });
+      
+})
 app.listen(3000, function(req, res){
     console.log('server listening at port no 3000')
 });
